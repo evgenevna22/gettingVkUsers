e2129872a0de9b89fa2c17312ea86eeaa628e262
Tatiana <tanya.evgenevna@gmail.com>
2019-12-22 20:51:04 +0300

    fix types of param and Observable

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -12,7 +12,7 @@ export class AppService {
 
   constructor(private http: HttpClient) { }
 
-  public getUserById(id: number | any): Observable<any> {
+  public getUserById(id: string): Observable<{}> {
     const params = {
       access_token: '1cf248f6e2de3ed7ee4939c0044b18db9966f5f35389fb2a84b0fa11b2b3b88dcd205b1c0e768c33544a0',
       user_ids: id,
